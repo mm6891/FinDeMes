63b1a3380097342859dbe2cd741af3d1aa6a8564
mm6891 <manuel.molero@gmail.com>
2015-02-18 17:37:08 +0100

    prueba subida proyecto github 18/02/15  16:00

--- app/src/main/java/globalsolutions/findemes/pantallas/GastoActivity.java
+++ app/src/main/java/globalsolutions/findemes/pantallas/GastoActivity.java
@@ -71,6 +71,28 @@ public class GastoActivity extends FragmentActivity implements DatePickerDialog.
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_gasto, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest. xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     //dialogos
     public void showDatePickerDialog(View v) {
--- app/src/main/java/globalsolutions/findemes/pantallas/MainActivity.java
+++ app/src/main/java/globalsolutions/findemes/pantallas/MainActivity.java
@@ -1,8 +1,10 @@
 package globalsolutions.findemes.pantallas;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +16,7 @@ import globalsolutions.findemes.database.model.GrupoIngreso;
 import globalsolutions.findemes.database.util.MyDatabaseHelper;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     //botonera menu
     private Button btnGasto;
@@ -55,6 +57,28 @@ public class MainActivity extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void CreaRegistros(){
