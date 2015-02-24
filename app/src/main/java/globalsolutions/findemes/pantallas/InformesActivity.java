4e2206e4889c9b3f7b7d57ca39408e64f2035515
mm6891 <manuel.molero@gmail.com>
2015-02-24 19:21:54 +0100

    subida 23/02    20:10

--- app/src/main/java/globalsolutions/findemes/pantallas/InformesActivity.java
+++ app/src/main/java/globalsolutions/findemes/pantallas/InformesActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 import globalsolutions.findemes.R;
+import globalsolutions.findemes.database.dao.MovimientoDAO;
+import globalsolutions.findemes.database.model.MovimientoItem;
 import globalsolutions.findemes.database.util.Constantes;
 
 /**
@@ -26,6 +29,7 @@ public class InformesActivity extends Activity{
     private Spinner spTipoMovimiento;
     private Spinner spPeriodo;
     private Spinner spAnyosInforme;
+    private ListView listViewMovsInforme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +53,17 @@ public class InformesActivity extends Activity{
         spTipoMovimiento.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, tiposMovimientos));
 
         //spinner periodo
+        ArrayList<String> periodos = new ArrayList<String>();
+        tiposMovimientos.add(Constantes.TIPO_FILTRO_RESETEO.toString());
+        tiposMovimientos.add(Constantes.TIPO_MOVIMIENTO_INGRESO.toString());
+        tiposMovimientos.add(Constantes.TIPO_MOVIMIENTO_GASTO.toString());
+        spPeriodo = (Spinner) findViewById(R.id.spPeriodo);
+        spTipoMovimiento.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, periodos));
 
+        //recuperamos movimientos
+        final ArrayList<MovimientoItem> movs = new MovimientoDAO().cargaMovimientos(getApplicationContext());
+        listViewMovsInforme = (ListView) findViewById(R.id.listViewMov);
+        listViewMovsInforme.setAdapter(new MovimientoAdapter(getApplicationContext(), movs));
     }
 
     @Override
