5c576e5ddcfd479c594f5e09c819ad95be703e56
mm6891 <manuel.molero@gmail.com>
2015-02-24 16:12:19 +0100

    prueba subida proyecto github 18/02/15  16:00

--- app/src/main/java/globalsolutions/findemes/pantallas/InformesActivity.java
+++ app/src/main/java/globalsolutions/findemes/pantallas/InformesActivity.java
@@ -1,10 +1,67 @@
 package globalsolutions.findemes.pantallas;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import globalsolutions.findemes.R;
+import globalsolutions.findemes.database.util.Constantes;
 
 /**
  * Created by Manuel on 23/02/2015.
  */
 public class InformesActivity extends Activity{
+
+    /*public enum Periodo {
+        PORDÍA = "Por día",
+        PORPERIODO = "Por período"
+    }*/
+
+    private Spinner spTipoMovimiento;
+    private Spinner spPeriodo;
+    private Spinner spAnyosInforme;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movimientos);
+
+        //boton retroceder
+        ImageButton btnReturn = (ImageButton) findViewById(R.id.btnBackButton);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                backActivity();
+            }
+        });
+
+        //spinner movimiento
+        ArrayList<String> tiposMovimientos = new ArrayList<String>();
+        tiposMovimientos.add(Constantes.TIPO_FILTRO_RESETEO.toString());
+        tiposMovimientos.add(Constantes.TIPO_MOVIMIENTO_INGRESO.toString());
+        tiposMovimientos.add(Constantes.TIPO_MOVIMIENTO_GASTO.toString());
+        spTipoMovimiento = (Spinner) findViewById(R.id.spTipoMovimiento);
+        spTipoMovimiento.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, tiposMovimientos));
+
+        //spinner periodo
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        backActivity();
+    }
+
+    private void backActivity(){
+        Intent in = new Intent(InformesActivity.this, MainActivity.class);
+        startActivity(in);
+        setResult(RESULT_OK);
+        finish();
+    }
 
 }
